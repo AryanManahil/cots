5e4b41175aad251e31d7e7fa273f6c6c92766312
AryanManahil <ivisionbd@yahoo.com>
2022-04-18 18:24:35 +0600

    com8

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import  Product
+from .models import  Product,Buyer,Supplier
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('order_no','buyer_name','product_name','order_qty','shipment_date')
 
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('buyer_name','buyer_contact_no','buyer_email','buyer_address')
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('supplier_name', 'supplier_contact_no', 'supplier_email', 'supplier_address')
+
+
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Buyer,BuyerAdmin)
+admin.site.register(Supplier,SupplierAdmin)
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -8,3 +8,15 @@ class Product(models.Model):
     order_qty = models.IntegerField()
     shipment_date = models.DateTimeField()
 
+class Buyer(models.Model):
+    buyer_name = models.CharField(max_length=255)
+    buyer_contact_no = models.CharField(max_length=100)
+    buyer_email = models.CharField(max_length=100)
+    buyer_address = models.CharField(max_length=255)
+
+class Supplier(models.Model):
+    supplier_name = models.CharField(max_length=255)
+    supplier_contact_no = models.CharField(max_length=100)
+    supplier_email = models.CharField(max_length=100)
+    supplier_address = models.CharField(max_length=255)
+
